c510676341fb3faba261092887d16cbb4a724df6
Stuart Clark <stu@rtclark.net>
2022-01-01 15:25:37 +1100

    fix(#42): update ddev install process (#43)

--- .gitpod/scripts/drupal-setup.sh
+++ .gitpod/scripts/drupal-setup.sh
@@ -4,8 +4,4 @@ set -eu -o pipefail
 DRUPAL_DIR="${GITPOD_REPO_ROOT}/drupal"
 
 # Set up Drupal website
-cd "$DRUPAL_DIR" && ddev composer install
-cd "$DRUPAL_DIR" && ddev drush si -y --account-pass=admin --site-name='quickstart-druxt-site' standard
-cd "$DRUPAL_DIR" && ddev drush -y en druxt
-cd "$DRUPAL_DIR" && ddev drush "rap anonymous 'access druxt resources'"
-cd "$DRUPAL_DIR" && ddev drush -y config:set jsonapi.settings read_only 0
+cd "$DRUPAL_DIR" && ddev drupal-install
