cf0da6f97cf2a9285c7578f35c740f92dd5be396
Stuart Clark <stu@rtclark.net>
2022-09-20 04:18:31 +0000

    chore: add unit tests

---
+++ nuxt/components/druxt/Logo.test.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import Logo from './Logo.vue'
+
+describe('Logo', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Logo, {})
+    expect(wrapper.vm).toBeTruthy()
+
+    // Expect HTML to match snapshot.
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
